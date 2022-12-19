66a4e67bb301d7d1522879fd0a23015684b4b3c8
salmanazmirafi <116106498+salmanazmirafi@users.noreply.github.com>
2022-12-19 15:53:22 +0600

    Update

--- DibeWutgit/App.js
+++ DibeWutgit/App.js
@@ -1,20 +1,20 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View, SafeAreaView } from "react-native";
 
 export default function App() {
+  console.log("APP Executed");
+
+
   return (
-    <View style={styles.container}>
+    <SafeAreaView style={styles.container}>
       <Text>Hello World! React Native</Text>
-      <StatusBar style="auto" />
-    </View>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
+    backgroundColor: "dodgerblue",
   },
 });
